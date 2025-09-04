a33f79c7e4e8bda00fb080bd94aa01389978d0ec
PPWangyc <ppwangyc@gmail.com>
2025-09-04 11:55:48 -0400

    fix run-create scripts

--- scripts/delta/run_create_ibl_encoding.sh
+++ scripts/delta/run_create_ibl_encoding.sh
@@ -3,6 +3,6 @@
 while IFS= read -r line
 do
     echo "Create dataset on ses eid: $line"
-    sbatch create_ibl_encoding.sh 1 $line
+    sbatch create_ibl_encoding.sh $line
     
 done < "../../data/encoding_test_eids.txt"
