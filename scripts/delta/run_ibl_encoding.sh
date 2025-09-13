63e266fe8c8dab6d9cba0dec085f5a1ed605b551
PPWangyc <ppwangyc@gmail.com>
2025-09-13 11:55:16 -0500

    revised

--- scripts/delta/run_ibl_encoding.sh
+++ scripts/delta/run_ibl_encoding.sh
@@ -5,6 +5,5 @@ avail_views=$2 # left+right
 while IFS= read -r line
 do
     echo "IBL-Encoding on ses eid: $line with model: $model and avail_views: $avail_views"
-    sbatch ibl_encoding.sh $line $model $avail_views
-
+    sbatch ibl_encoding.sh $line $model "$avail_views"
 done < "../../data/encoding_test_eids.txt"
